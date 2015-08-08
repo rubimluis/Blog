64507eab791cbe37bf6115bda621bff5de5bcacf
rubimluis <rubimluis@hotmail.com>
2015-08-08 01:43:01 -0300

    comentários das alterações

---
+++ Business.Entitie/PostTag.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Entitie
+{
+    [Table("PostTag")]
+    public class PostTag
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public Guid IdPost { get; set; }
+        [ForeignKey("IdPost")]
+        public virtual Post Post { get; set; }
+
+        public Guid IdTag { get; set; }
+        [ForeignKey("IdTag")]
+        public virtual Tag Tag { get; set; }
+
+    }
+}
